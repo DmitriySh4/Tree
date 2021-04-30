8bee108edda38f1e901f433d9b2aa0e9310172f7
dmitryshvets <shvets@outsourcing-ukraine.com>
2021-04-30 16:09:15 +0300

    Further amendments

--- Helpers/AjaxHandler.php
+++ Helpers/AjaxHandler.php
@@ -1,24 +1,21 @@
 <?php 
 include '../Controllers/TreeController.php';
-	
+	$tree = new TreeController();
+
 	if (isset($_POST['delete_id'])) {
-		$id = $_POST['delete_id'];
-		$tree = new TreeController();
+		$id = $_POST['delete_id'];		
 		$tree->DeleteRoot($_POST['delete_id']);
 	}
 
 	if (isset($_POST['text']) && isset($_POST['parents'])) {
-		$tree = new TreeController();
 		$tree->CreateRoot($_POST['text'], $_POST['position'], implode(",", $_POST['parents']));
 	}
 
 	if (isset($_GET['getRoots'])) {
-		$tree = new TreeController();
 		$tree->GetRoots();
 	}
 
 	if (isset($_POST['edit_id'])) {
-		$tree = new TreeController();
 		$tree->UpdateRoot($_POST['textEdit'], $_POST['edit_id']);
 	}
 ?>
